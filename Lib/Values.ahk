JoyHak/QuickSwitch

commit 6c3cb5a4e8f6db4e20c19face750e2dcb6e3ebd9

Refactored Values lib (#85)
### Refactored

- Refactored Read / Write values to reduce disk usage.
- Refactored all validators to improve readability and reduce disk usage.
- App works better if config cant be readed / created.
- Removed legacy syntax from multi-line sections.
- Added detailed comments to XY script.
- Added ValidateFile() to create detailed reports about configs and TotalCmd tabs.
- Improved documentation for all Values lib functions.

### Fixed

- Turning off "close old dialog" option after opening General dialog.
- Usage of wrong values entered by user in settings.
- Empty values are no longer written to the INI, which no longer causes errors.

Lib/Values.ahk
@@ -27,8 +27,21 @@ SetDefaultValues() {
     */
     global
 
-    MainKeyHook := ShowNoSwitch := ShowAfterSettings := AutoStartup := true
-    RestartKeyHook := AutoSwitch := ShowAlways := ShowAfterSelect := CloseDialog := PathNumbers := ShortPath := ShortenEnd := ShowDriveLetter := ShowFirstSeparator := false
+    AutoStartup         :=  true
+    MainKeyHook         :=  true
+    ShowNoSwitch        :=  true
+    ShowAfterSettings   :=  true
+
+    AutoSwitch          :=  false
+    ShowAlways          :=  false
+    ShowAfterSelect     :=  false
+    RestartKeyHook      :=  false
+    CloseDialog         :=  false
+    PathNumbers         :=  false
+    ShortPath           :=  false
+    ShortenEnd          :=  false
+    ShowDriveLetter     :=  false
+    ShowFirstSeparator  :=  false
 
     GuiColor := MenuColor := ""
 
@@ -37,206 +50,233 @@ SetDefaultValues() {
     DirNameLength  := 20
     PathSeparator  := "\"
 
+    RestartWhere   := "ahk_exe notepad++.exe"
     MainFont       := "Tahoma"
     MainKey        := "^sc10"
     RestartKey     := "^sc1F"
-    RestartWhere   := "ahk_exe notepad++.exe"
 }
 
 ;─────────────────────────────────────────────────────────────────────────────
 ;
 WriteValues() {
 ;─────────────────────────────────────────────────────────────────────────────
-    /*
-        Calls validators and writes values to INI.
+      /*
+          Calls validators and writes values to INI
 
-        The status of the checkboxes from the settings menu is writed immediately.
-        Strings and colors from fields are checked before writing.
-
-        File, section, param name, global var and its value reference
-        must be identical to ReadValues()
-    */
+          The boolean (checkbox) values is writed immediately.
+          The individual special values are checked before writing.
+      */
     global
 
+    Values := "
+    (LTrim
+         AutoStartup="          AutoStartup           "
+         AutoSwitch="           AutoSwitch            "
+         ShowAlways="           ShowAlways            "
+         ShowNoSwitch="         ShowNoSwitch          "
+         ShowAfterSelect="      ShowAfterSelect       "
+         ShowAfterSettings="    ShowAfterSettings     "
+         CloseDialog="          CloseDialog           "
+         PathNumbers="          PathNumbers           "
+         ShortPath="            ShortPath             "
+         ShortenEnd="           ShortenEnd            "
+         ShowDriveLetter="      ShowDriveLetter       "
+         ShowFirstSeparator="   ShowFirstSeparator    "
+         DirsCount="            DirsCount             "
+         DirNameLength="        DirNameLength         "
+         MainFont="             MainFont              "
+         RestartWhere="         RestartWhere          "
+         MainKeyHook="          MainKeyHook           "
+         RestartKeyHook="       RestartKeyHook        "
+    )"
+
+    Values .= "`n"
+            . ValidateTrayIcon( "MainIcon",             MainIcon)
+            . ValidateColor(    "GuiColor",             GuiColor)
+            . ValidateColor(    "MenuColor",            MenuColor)
+            . ValidateString(   "PathSeparator",        PathSeparator)
+            . ValidateString(   "ShortNameIndicator",   ShortNameIndicator)
+            . ValidateKey(      "MainKey",              MainKey,            MainKeyHook,        "Off",      "ShowMenu")
+            . ValidateKey(      "RestartKey",           RestartKey,         RestartKeyHook,     "On",       "RestartApp")
+
     try {
-        ;           value                       INI name    section     param name
-        IniWrite,   %AutoStartup%,              %INI%,      App,        AutoStartup
-        IniWrite,   %MainFont%,                 %INI%,      App,        MainFont
-        IniWrite,   %RestartWhere%,             %INI%,      App,        RestartWhere
-        IniWrite,   %MainKeyHook%,              %INI%,      App,        MainKeyHook
-        IniWrite,   %RestartKeyHook%,           %INI%,      App,        RestartKeyHook
-        IniWrite,   %LastTabSettings%,          %INI%,      App,        LastTabSettings
-        IniWrite,   %AutoSwitch%,               %INI%,      Menu,       AutoSwitch
-        IniWrite,   %ShowAlways%,               %INI%,      Menu,       ShowAlways
-        IniWrite,   %ShowNoSwitch%,             %INI%,      Menu,       ShowNoSwitch
-        IniWrite,   %ShowAfterSettings%,        %INI%,      Menu,       ShowAfterSettings
-        IniWrite,   %ShowAfterSelect%,          %INI%,      Menu,       ShowAfterSelect
-        IniWrite,   %CloseDialog%,              %INI%,      Menu,       CloseDialog
-        IniWrite,   %ShortPath%,                %INI%,      Menu,       ShortPath
-        IniWrite,   %PathNumbers%,              %INI%,      Menu,       PathNumbers
-        IniWrite,   %ShowDriveLetter%,          %INI%,      Menu,       ShowDriveLetter
-        IniWrite,   %ShortenEnd%,               %INI%,      Menu,       ShortenEnd
-        IniWrite,   %ShowFirstSeparator%,       %INI%,      Menu,       ShowFirstSeparator
-        IniWrite,   %DirsCount%,                %INI%,      Menu,       DirsCount
-        IniWrite,   %DirNameLength%,            %INI%,      Menu,       DirNameLength
+        IniWrite, % Values, % INI, Global
     } catch {
-        LogError(Exception("Failed to write values to the configuration"
-                            , INI . " write"
-                            , "Create INI file manually or change the INI global variable"))
+        LogError(Exception("Please create this file with UTF-16 LE BOM encoding manually: `'" INI "`'"
+                           , "config"
+                           , ValidateFile(INI)))
     }
-
-    ValidateWriteString(PathSeparator,      "PathSeparator")
-    ValidateWriteString(ShortNameIndicator, "ShortNameIndicator")
-
-    ValidateWriteKey(MainKey,       "MainKey",    "ShowMenu",   "Off",  MainKeyHook)
-    ValidateWriteKey(RestartKey,    "RestartKey", "RestartApp", "On",   RestartKeyHook)
-
-    ValidateWriteColor(GuiColor,    "GuiColor")
-    ValidateWriteColor(MenuColor,   "MenuColor")
-    ValidateWriteTrayIcon(MainIcon, "MainIcon")
+    Values := ""
 }
 
 ;─────────────────────────────────────────────────────────────────────────────
 ;
 ReadValues() {
 ;─────────────────────────────────────────────────────────────────────────────
-    /*
-        Reads values from INI.
-
-        All global variables are updated if:
-        - the configuration exists
-        - values exist in the configuration
-        - variables have been declared
-
-        File, section, param name, global var and its value reference
-        must be identical to WriteValues()
-    */
+    ; Reads values from INI
     global
 
-    ;           global                      INI name    section     param name                  default value
-    IniRead,    AutoStartup,                %INI%,      App,        AutoStartup,                %AutoStartup%
-    IniRead,    MainKey,                    %INI%,      App,        MainKey,                    %MainKey%
-    IniRead,    MainFont,                   %INI%,      App,        MainFont,                   %MainFont%
-    IniRead,    RestartKey,                 %INI%,      App,        RestartKey,                 %RestartKey%
+    if !FileExist(INI)
+        return
 
-    IniRead,    MainKeyHook,                %INI%,      App,        MainKeyHook,                %MainKeyHook%
-    IniRead,    RestartKeyHook,             %INI%,      App,        RestartKeyHook,             %RestartKeyHook%
-    IniRead,    RestartWhere,               %INI%,      App,        RestartWhere,               %RestartWhere%
-    IniRead,    LastTabSettings,            %INI%,      App,        LastTabSettings,            %LastTabSettings%
-
-    IniRead,    AutoSwitch,                 %INI%,      Menu,       AutoSwitch,                 %AutoSwitch%
-    IniRead,    ShowAlways,                 %INI%,      Menu,       ShowAlways,                 %ShowAlways%
-    IniRead,    ShowNoSwitch,               %INI%,      Menu,       ShowNoSwitch,               %ShowNoSwitch%
-    IniRead,    ShowAfterSettings,          %INI%,      Menu,       ShowAfterSettings,          %ShowAfterSettings%
-    IniRead,    ShowAfterSelect,            %INI%,      Menu,       ShowAfterSelect,            %ShowAfterSelect%
-    IniRead,    CloseDialog,                %INI%,      Menu,       CloseDialog,                %CloseDialog%
-    IniRead,    ShortPath,                  %INI%,      Menu,       ShortPath,                  %ShortPath%
-    IniRead,    PathNumbers,                %INI%,      Menu,       PathNumbers,                %PathNumbers%
-    IniRead,    ShowDriveLetter,            %INI%,      Menu,       ShowDriveLetter,            %ShowDriveLetter%
-    IniRead,    ShortenEnd,                 %INI%,      Menu,       ShortenEnd,                 %ShortenEnd%
-    IniRead,    ShowFirstSeparator,         %INI%,      Menu,       ShowFirstSeparator,         %ShowFirstSeparator%
-
-    IniRead,    DirsCount,                  %INI%,      Menu,       DirsCount,                  %DirsCount%
-    IniRead,    DirNameLength,              %INI%,      Menu,       DirNameLength,              %DirNameLength%
-
-    IniRead,    PathSeparator,              %INI%,      Menu,       PathSeparator,              %PathSeparator%
-    IniRead,    ShortNameIndicator,         %INI%,      Menu,       ShortNameIndicator,         %ShortNameIndicator%
-
-    IniRead,    MainIcon,                   %INI%,      App,        MainIcon,                   %A_Space%
-    IniRead,    GuiColor,                   %INI%,      Colors,     GuiColor,                   %A_Space%
-    IniRead,    MenuColor,                  %INI%,      Colors,     MenuColor,                  %A_Space%
+    try {
+        IniRead, Values, % INI, Global
+        for Variable, Value in Object(StrSplit(Values, ["`n", "="]))
+            %Variable% := Value
+    }
+    Values := ""
 }
 
 ;─────────────────────────────────────────────────────────────────────────────
 ;
-ValidateWriteKey(ByRef sequence, ByRef paramName, ByRef funcName := "", ByRef state := "On", ByRef useHook := false) {
+ValidateKey(ByRef paramName, ByRef sequence, ByRef useHook, ByRef state := "On", ByRef funcName := "") {
 ;─────────────────────────────────────────────────────────────────────────────
+    /*
+        Replaces chars / letters in sequence with
+        standard modifiers ! ^ + #
+        and SC codes, e.g. Q -> sc10
+
+        If converted, returns the string of the form "paramName=result",
+        otherwise returns empty string
+    */
     global INI
 
     try {
-        ; Convert sequence to Scan Codes (if not converted)
-        if !(sequence ~= "i)sc[a-f0-9]+") {
+        if (sequence ~= "i)sc[a-f0-9]+") {
+            _key := sequence
+        } else {
+            ; Convert sequence to Scan Codes (if not converted)
             _key := ""
             Loop, parse, sequence
             {
                 if (!(A_LoopField ~= "[\!\^\+\#<>]")
                     && _scCode := GetKeySC(A_LoopField)) {
+                    ; Not a modifier, found scancode
                     _key .= Format("sc{:x}", _scCode)
                 } else {
+                    ; Don't change
                     _key .= A_LoopField
                 }
             }
-        } else {
-            _key := sequence
         }
 
         _prefix := useHook ? "" : "~"
         if funcName {
-            ; Create new hotkey
+            ; Register new hotkey
             Hotkey, % _prefix . _key, % funcName, % state
 
             try {
                 ; Remove old if exist
-                IniRead, _old, % INI, App, % paramName, % _key
+                IniRead, _old, % INI, Global, % paramName, % _key
                 if (_old != _key) {
                     Hotkey, % "~" . _old, Off
                     Hotkey, % _old, Off
                 }
-                IniWrite, % _key, % INI, App, % paramName
             }
 
         } else {
             ; Set state for existing hotkey
             Hotkey, % _prefix . _key, % state
         }
+        return paramName "=" _key "`n"
 
     } catch _error {
         LogError(_error)
     }
+    return ""
 }
 
 ;─────────────────────────────────────────────────────────────────────────────
 ;
-ValidateWriteColor(ByRef color, ByRef paramName) {
+ValidateColor(ByRef paramName, ByRef color) {
 ;─────────────────────────────────────────────────────────────────────────────
-    global INI
+    /*
+        Searches for a HEX number in any form, e.g. 0x, #, h
 
-    if !color {
-        try IniWrite, % A_Space, % INI, Colors, % paramName
-        return
+        If found, returns the string of the form "paramName=result",
+        otherwise returns empty string
+    */
+
+    if !color
+        return ""
+
+    if !(_matchPos := RegExMatch(color, "i)[a-f0-9]{6}$")) {
+        LogError(Exception("`'" color "`' is wrong color! Enter the HEX value", paramName))
+        return ""
     }
 
-    if !(_matchPos := RegExMatch(color, "i)[a-f0-9]{6}$"))
-        return LogError(Exception("`'" color "`' is wrong color! Enter the HEX value", paramName))
-
-    _result := SubStr(color, _matchPos)
-    try IniWrite, % _result, % INI, Colors, % paramName
+    return paramName . "=" . SubStr(color, _matchPos) . "`n"
 }
 
 ;─────────────────────────────────────────────────────────────────────────────
 ;
-ValidateWriteString(ByRef string, ByRef paramName) {
+ValidateString(ByRef paramName, ByRef string) {
 ;─────────────────────────────────────────────────────────────────────────────
-    global INI
+    /*
+        Converts input value to string
 
-    _result := Format("{}", string)
-    try IniWrite, % _result, % INI, Menu, % paramName
+        If not empty, returns the string of the form "paramName=result",
+        otherwise returns empty string
+    */
+
+    if !string
+        return ""
+
+    return paramName . "=" . Format("{}", string) . "`n"
 }
 
 ;─────────────────────────────────────────────────────────────────────────────
 ;
-ValidateWriteTrayIcon(ByRef icon, ByRef paramName) {
+ValidateTrayIcon(ByRef paramName, ByRef icon) {
 ;─────────────────────────────────────────────────────────────────────────────
-    global INI, MainIcon
+    /*
+        If the file exists, changes the tray icon
+        and returns a string of the form "paramName=result",
+        otherwise returns empty string
+    */
 
-    if !icon {
-        try IniWrite, % A_Space, % INI, App, % paramName
-        return
+    if !icon
+        return ""
+
+    if !FileExist(icon) {
+        LogError(Exception("Icon `'" icon "`' not found", "tray icon", "Specify the full path to the file"))
+        return ""
     }
 
-    if !FileExist(icon)
-        return LogError(Exception("Icon `'" icon "`' not found", "tray icon", "Specify the full path to the file"))
+    Menu, Tray, Icon, % icon
+    return paramName "=" icon "`n"
+}
 
-    Menu, Tray, Icon, %MainIcon%
-    try IniWrite, % icon, % INI, App, % paramName
+;─────────────────────────────────────────────────────────────────────────────
+;
+ValidateFile(ByRef filePath) {
+;─────────────────────────────────────────────────────────────────────────────
+    _extra := "Cant write data to the file"
+
+    if !filePath {
+        _extra := "File path is empty: `'" filePath "`'"
+    } else if !FileExist(filePath) {
+        _extra := "Unable to create file"
+    } else {
+        _file := FileOpen(filePath, "r")
+
+        if !IsObject(_file) {
+            FileGetAttrib, _attr, % filePath
+            _extra := "Unable to get access to the file"
+        } else {
+            _extra     := "`nRead existing `'" filePath "`'`n"
+
+            _firstLine := RTrim(_file.readLine(), " `r`n")
+            _extra     .= Format("Encoding: {} First line: {}, Size in bytes: {} HWND: {}`n"
+                                 , _file.encoding, _firstLine, _file.length, _file.handle)
+        }
+        _file.Close()
+
+        try {
+            FileGetAttrib, _attr, % filePath
+            _extra .= "File attributes: " _attr
+        }
+    }
+
+    return _extra "`n"
 }

Lib/FileDialogs.ahk
@@ -70,9 +70,11 @@ FeedDialogGENERAL(ByRef winId, ByRef path) {
     global CloseDialog
 
     ; Always send {Enter}
+    _closeDialog :=  CloseDialog
     CloseDialog  :=  true
+
     _result      :=  FeedDialogSYSTREEVIEW(winId, path)
-    CloseDialog  :=  false
+    CloseDialog  :=  _closeDialog
 
     return _result
 }

Lib/Log.ahk
@@ -45,21 +45,23 @@ LogInfo(ByRef msg, _silent := false) {
 
 LogHeader() {
     ; Header about log and OS
-    global ErrorsLog, BugReportLink, ScriptName
+    global ErrorsLog, ScriptName
 
-    static reg := "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
-    RegRead, _OSname, % reg, ProductName
-    RegRead, _OSversion, % reg, DisplayVersion
-    RegRead, _OSbuild, % reg, CurrentBuild
+    static REPORT_LINK  :=  "https://github.com/JoyHak/QuickSwitch/issues/new?template=bug-report.yaml"
+    static LEAF         :=  "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+
+    RegRead, _OSname, % LEAF, ProductName
+    RegRead, _OSversion, % LEAF, DisplayVersion
+    RegRead, _OSbuild, % LEAF, CurrentBuild
     RegRead, _lang, HKEY_CURRENT_USER\Control Panel\International, LocaleName
 
-    FileAppend,
+    FileAppend, % "
     (LTrim
-     Report about error: %BugReportLink%
-     AHK %A_AhkVersion%
-     %_OSname% %_OSversion% | %_OSbuild% %_lang%
+        Report about error: " REPORT_LINK "
+        AHK " A_AhkVersion "
+        " _OSname " " _OSversion " | " _OSbuild " " _lang "
 
-    ), % ErrorsLog
+    )", % ErrorsLog
 }
 
 LogVersion() {
@@ -79,7 +81,7 @@ LogVersion() {
     FileAppend, % _header, % ErrorsLog
 }
 
-ValidateLog() {
+InitLog() {
     global INI, ErrorsLog, ScriptName
 
     if FileExist(ErrorsLog) {
@@ -93,7 +95,7 @@ ValidateLog() {
     if !FileExist(ErrorsLog) {
         LogHeader()
         LogVersion()
-        Return
+        return
     }
 
     ; does the cur. dir. match the dir. of the script that previously created this log?

Lib/ManagerClasses.ahk
@@ -37,20 +37,7 @@ CabinetWClass(ByRef winId) {
 ;
 ThunderRT6FormDC(ByRef winId) {
 ;─────────────────────────────────────────────────────────────────────────────
-    /*
-        Sends script to XYplorer and parses the clipboard.
-
-        If the second panel is enabled, gets tabs from all panels,
-        otherwise gets tabs from the active panel.
-        All native variables are resolved.
-
-        The path separator is |
-        For each path, gets the real path (XY has special and virtual paths).
-        Removes the extra | from the beginning of $reals
-
-        Places $reals on the clipboard.
-        Parses it and puts all paths into the global array
-    */
+    ; Sends script to XYplorer and parses the clipboard.
     global Paths
 
     try {
@@ -59,17 +46,17 @@ ThunderRT6FormDC(ByRef winId) {
         Clipboard  := ""
 
         static script := "
-        ( LTrim Join
-            ::$paths = <get tabs_sf | a>, 'r'`;
-            if (get('#800')) {
-                $paths .= '|' . <get tabs_sf | i>`;
+        ( LTrim Join Comments
+            ::$paths = <get tabs_sf | a>, 'r'`;         ; Get tabs from the active panel, resolve native variables
+            if (get('#800')) {                          ; Second panel is enabled
+                $paths .= '|' . <get tabs_sf | i>`;     ; Get tabs from second panels
             }
             $reals = ''`;
-            foreach($path, $paths, '|') {
-                $reals .= '|' . pathreal($path)`;
+            foreach($path, $paths, '|') {               ; Path separator is |
+                $reals .= '|' . pathreal($path)`;       ; Get the real path (XY has special and virtual paths)
             }
-            $reals = trim($reals, '|', 'L')`;
-            copytext $reals`;
+            $reals = trim($reals, '|', 'L')`;           ; Remove the extra  | from the beginning of $reals
+            copytext $reals`;                           ; Place $reals to the clipboard, faster then copydata
         )"
 
         SendXyplorerScript(winId, script)

Lib/SettingsBackend.ahk
@@ -32,7 +32,8 @@ SaveSettings() {
     ; Read current GUI (global) values
     Gui, Submit
     WriteValues()
-    ValidateAutoStartup()
+    ReadValues()
+    InitAutoStartup()
 }
 
 RestartApp() {
@@ -81,19 +82,18 @@ ToggleShortPath() {
 
 ;─────────────────────────────────────────────────────────────────────────────
 ;
-ValidateAutoStartup() {
+InitAutoStartup() {
 ;─────────────────────────────────────────────────────────────────────────────
-    global AutoStartup, ScriptName, INI
+    global AutoStartup, ScriptName
 
     try {
-        IniRead, AutoStartup, % INI, App, AutoStartup, % AutoStartup
-        link := A_Startup . "\" . ScriptName . ".lnk"
+        _link := A_Startup . "\" . ScriptName . ".lnk"
 
         if AutoStartup {
-            FileCreateShortcut, % A_ScriptFullPath, % link, % A_ScriptDir
+            FileCreateShortcut, % A_ScriptFullPath, % _link, % A_ScriptDir
         } else {
-            if FileExist(link) {
-                FileDelete, % link
+            if FileExist(_link) {
+                FileDelete, % _link
                 TrayTip, % ScriptName, AutoStartup disabled,, 0x2
             }
         }

Lib/TotalCommander/Create.ahk
@@ -1,7 +1,7 @@
 CreateTotalUserCommand(ByRef ini, ByRef cmd, ByRef internalCmd, ByRef param := "") {
     ; Creates cmd in specified ini config.
     ; "cmd" param must start with EM_
-    
+
     try {
         loop, 4 {
             ; Read the contents of the config until it appears or the loop ends with an error
@@ -22,20 +22,23 @@ CreateTotalUserCommand(ByRef ini, ByRef cmd, ByRef internalCmd, ByRef param := "
             }
 
             ; Create new section
-            FileAppend,
+            FileAppend, % "
             (LTrim
-            # Please dont add commands with the same name
-            [%cmd%]
-            cmd=%internalCmd%
-            param=%param%
+                # Please dont add commands with the same name
+                [" cmd "]
+                cmd="   internalCmd  "
+                param=" param        "
 
-            ), % ini
+            )", % ini
 
             sleep, 50 * A_Index
         }
         throw Exception("Unable to create configuration", "")
 
     } catch _e {
-        throw Exception("Please create this file manually: `'" ini "`'", "TotalCmd config", _e.what " " _e.message " " _e.extra)
+        throw Exception("Please create this file manually: `'" ini "`'"
+                        , "TotalCmd config"
+                        , _e.what " " _e.message " " _e.extra "`n"
+                        . ValidateFile(ini))
     }
 }

Lib/TotalCommander/Tabs.ahk
@@ -34,5 +34,8 @@ ParseTotalTabs(ByRef tabsFile) {
         }
         sleep, 20
     }
-    throw Exception("Unable to access tabs", "TotalCmd tabs", "Restart Total Commander and retry")
+    throw Exception("Unable to access tabs"
+                    , "TotalCmd tabs"
+                    , "Restart Total Commander and retry`n"
+                    . ValidateFile(tabsFile))
 }

QuickSwitch-1.4.10.ahk
@@ -30,7 +30,6 @@ ScriptName    := "QuickSwitch"
 MainIcon      := ""
 INI           := ScriptName ".ini"
 ErrorsLog     := "Errors.log"
-BugReportLink := "https://github.com/JoyHak/QuickSwitch/issues/new?template=bug-report.yaml"
 
 #Include <Log>
 #Include <Debug>
@@ -49,19 +48,19 @@ BugReportLink := "https://github.com/JoyHak/QuickSwitch/issues/new?template=bug-
 #Include <SettingsFrontend>
 #Include <MenuFrontend>
 
-ValidateLog()
+InitLog()
 
 ;@Ahk2Exe-IgnoreBegin
 MainIcon := "QuickSwitch.ico"
-ValidateWriteTrayIcon(MainIcon, "MainIcon")
+ValidateTrayIcon("MainIcon", MainIcon)
 ;@Ahk2Exe-IgnoreEnd
 
 SetDefaultValues()
 ReadValues()
-ValidateAutoStartup()
+InitAutoStartup()
 
-ValidateWriteKey(MainKey,    "MainKey",    "ShowMenu",   "Off", MainKeyHook)
-ValidateWriteKey(RestartKey, "RestartKey", "RestartApp", "On",  RestartKeyHook)
+ValidateKey("MainKey",    MainKey,    MainKeyHook,    "Off", "ShowMenu")
+ValidateKey("RestartKey", RestartKey, RestartKeyHook, "On",  "RestartApp")
 
 Loop {
     ; Wait for any "Open/Save as" file dialog
@@ -85,7 +84,7 @@ Loop {
             GetPaths()
 
             ; Turn on hotkey to hide / show menu later
-            ValidateWriteKey(MainKey, "MainKey",, "On", MainKeyHook)
+            ValidateKey("MainKey", MainKey, MainKeyHook, "On")
 
             if IsMenuReady()
                 ShowMenu()
@@ -99,7 +98,7 @@ Loop {
 
     Sleep, 100
     WinWaitNotActive
-    ValidateWriteKey(MainKey, "MainKey",, "Off", MainKeyHook)
+    ValidateKey("MainKey", MainKey, MainKeyHook, "Off")
 
     ; Clean up
     if (SaveDialogAction && FingerPrint && DialogAction != "") {

commit 38ab3048a804c7f239dd2b5b2263b9f2f4fbcba5

Fix ReadValues() (6c3cb5a4e8f6db4e20c19face750e2dcb6e3ebd9)
Was broken after 6c3cb5a4e8f6db4e20c19face750e2dcb6e3ebd9

Lib/Values.ahk
@@ -68,7 +68,7 @@ WriteValues() {
       */
     global
 
-    Values := "
+    local _values := "
     (LTrim
          AutoStartup="          AutoStartup           "
          AutoSwitch="           AutoSwitch            "
@@ -90,7 +90,7 @@ WriteValues() {
          RestartKeyHook="       RestartKeyHook        "
     )"
 
-    Values .= "`n"
+    _values .= "`n"
             . ValidateTrayIcon( "MainIcon",             MainIcon)
             . ValidateColor(    "GuiColor",             GuiColor)
             . ValidateColor(    "MenuColor",            MenuColor)
@@ -100,13 +100,12 @@ WriteValues() {
             . ValidateKey(      "RestartKey",           RestartKey,         RestartKeyHook,     "On",       "RestartApp")
 
     try {
-        IniWrite, % Values, % INI, Global
+        IniWrite, % _values, % INI, Global
     } catch {
         LogError(Exception("Please create this file with UTF-16 LE BOM encoding manually: `'" INI "`'"
                            , "config"
                            , ValidateFile(INI)))
     }
-    Values := ""
 }
 
 ;─────────────────────────────────────────────────────────────────────────────
@@ -118,13 +117,17 @@ ReadValues() {
 
     if !FileExist(INI)
         return
-
-    try {
-        IniRead, Values, % INI, Global
-        for Variable, Value in Object(StrSplit(Values, ["`n", "="]))
-            %Variable% := Value
+    
+    local _values, _array, _variable, _value
+    IniRead, _values, % INI, Global
+    
+    Loop, Parse, % _values, `n
+    {
+          _array       :=  StrSplit(A_LoopField, "=")
+          _variable    :=  _array[1]
+          _value       :=  _array[2]
+          %_variable%  :=  _value
     }
-    Values := ""
 }
 
 ;─────────────────────────────────────────────────────────────────────────────